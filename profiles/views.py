7d38dece60b993da396cd4c9ca4d9308b143a9c5
RyandTeague <ryanteague@hotmail.co.uk>
2023-02-11 22:20:27 +0000

    add profile view and serializers

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.db.models import Count
+from rest_framework import generics, filters
+from drf_tsk.permissions import IsOwnerOrReadOnly
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileList(generics.ListAPIView):
+    """
+    List all profiles.
+    No create view as profile creation is handled by django signals.
+    """
+    queryset = Profile.objects.annotate(
+    project_count=Count('owner__project', distinct=True),
+    serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'project_count',
+    ]
+
+
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.annotate(
+    project_count=Count('owner__project', distinct=True),
+
+    serializer_class = ProfileSerializer
