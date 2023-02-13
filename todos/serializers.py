6721eb3ed9b4d4b4e0843bc99f66a9e300f401ca
RyandTeague <ryanteague@hotmail.co.uk>
2023-02-13 21:35:06 +0000

    customize serializers

--- todos/serializers.py
+++ todos/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models import Todo
 
 class TodoSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
 	class Meta:
 		model = Todo
-		fields = ('id', 'title', 'description', 'completed')
+		fields = ('id', 'title', 'description', 'completed',
+            'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            )
