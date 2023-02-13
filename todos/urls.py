9bfd96d37d0503a4e4f05afb63c85e1a685fd703
RyandTeague <ryanteague@hotmail.co.uk>
2023-02-13 21:28:32 +0000

    customize model

--- todos/urls.py
+++ todos/urls.py
@@ -1,5 +1,13 @@
 from rest_framework import routers
-from .views import TodoViewset
+from .views import Todo
 
 router = routers.DefaultRouter()
 router.register('todo', TodoViewset)
+
+from django.urls import path
+from profiles import views
+
+urlpatterns = [
+    path('todo/', views.Todo.as_view()),
+    path('todo/<int:pk>/', views.ToDoDetail.as_view())
+]
--- todos/views.py
+++ todos/views.py
@@ -2,6 +2,6 @@ from rest_framework import viewsets
 from .models import Todo
 from .serializers import TodoSerializer
 
-class TodoViewset(viewsets.ModelViewSet):
+class Todo(viewsets.ModelViewSet):
 	queryset = Todo.objects.all()
 	serializer_class = TodoSerializer
