061b88401dbeb4f6374824fb03d4bf57b3cc75d8
RyandTeague <ryanteague@hotmail.co.uk>
2023-02-13 21:56:07 +0000

    customize code and set up backend

--- todos/urls.py
+++ todos/urls.py
@@ -1,13 +1,6 @@
-from rest_framework import routers
-from .views import Todo
-
-router = routers.DefaultRouter()
-router.register('todo', TodoViewset)
-
 from django.urls import path
-from profiles import views
+from todos import views
 
 urlpatterns = [
     path('todo/', views.Todo.as_view()),
-    path('todo/<int:pk>/', views.ToDoDetail.as_view())
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,12 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
+from drf_tsk.permissions import IsOwnerOrReadOnly
 from .models import Todo
-from .serializers import TodoSerializer
+from .serializers import ToDoSerializer
 
 class Todo(generics.ListAPIView):
-	queryset = Todo.objects.all()
-	serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Todo.objects.all()
+    serializer_class = ToDoSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
