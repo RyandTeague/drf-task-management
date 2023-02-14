b0452102a3dc2f7bcec4af567014446adc56a451
RyandTeague <ryanteague@hotmail.co.uk>
2023-02-14 00:19:55 +0000

    edit settings for heroku

--- drf_tsk/settings.py
+++ drf_tsk/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost', 'https://task-backend.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost', 'task-backend.herokuapp.com']
 
 
 # Application definition
