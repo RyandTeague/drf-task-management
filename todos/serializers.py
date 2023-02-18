4a5df70aa1b7efbca7b3c7b0338b13b8244f08ad
RyandTeague <ryanteague@hotmail.co.uk>
2023-02-18 18:57:19 +0000

    comment out code in serializer

--- todos/serializers.py
+++ todos/serializers.py
@@ -4,8 +4,8 @@ from .models import Todo
 class ToDoSerializer(serializers.ModelSerializer):
 #    owner = serializers.ReadOnlyField(source='owner.username')
 #    is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+#    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+#    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
 #    def get_is_owner(self, obj):
 #        request = self.context['request']
