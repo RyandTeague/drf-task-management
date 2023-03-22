ae5a924853a648bddd5c5e34c145127cca00a742
RyandTeague <ryanteague@hotmail.co.uk>
2023-03-22 13:11:26 +0000

    remove authentication from groups

--- groups/views.py
+++ groups/views.py
@@ -6,7 +6,6 @@ from .serializers import GroupSerializer
 
 class GroupList(generics.CreateAPIView):
     serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -15,7 +14,6 @@ class GroupList(generics.CreateAPIView):
 class GroupDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = GroupSerializer
     queryset = Group.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return self.queryset.filter(owner=self.request.user)
