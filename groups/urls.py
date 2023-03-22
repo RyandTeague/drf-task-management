816fc316235eabc5c76de78fa9910c8962e4ccbf
RyandTeague <ryanteague@hotmail.co.uk>
2023-03-22 13:20:31 +0000

    remove authentication from groups

--- groups/urls.py
+++ groups/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from groups import views
 
 urlpatterns = [
-    path('groups/', views.GroupList.as_view()),
-    path('groups/<int:pk>/', views.GroupDetail.as_view())
+    path('groups/', views.GroupList.as_view(), name="group-list"),
+    path('groups/<int:pk>/', views.GroupDetail.as_view(), name="group-detail")
 ]
--- groups/views.py
+++ groups/views.py
@@ -6,7 +6,9 @@ from .serializers import GroupSerializer
 
 class GroupList(generics.CreateAPIView):
     serializer_class = GroupSerializer
-
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Todo.objects.all().order_by('-created_at')
+    
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
@@ -14,6 +16,7 @@ class GroupList(generics.CreateAPIView):
 class GroupDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = GroupSerializer
     queryset = Group.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         return self.queryset.filter(owner=self.request.user)
