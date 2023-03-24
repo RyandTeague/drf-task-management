e84ea63494fb3602338e81c51b6d8e1c4856dee4
RyandTeague <ryanteague@hotmail.co.uk>
2023-03-24 00:50:53 +0000

    fix groups urls

--- groups/urls.py
+++ groups/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import GroupList, GroupDetail
 
 urlpatterns = [
-    path('groups/', views.GroupList.as_view(), name="group-list"),
-    path('groups/<int:pk>/', views.GroupDetail.as_view(), name="group-detail")
+    path('groups/', GroupList.as_view(), name="group-list"),
+    path('groups/<int:pk>/', GroupDetail.as_view(), name="group-detail")
 ]
