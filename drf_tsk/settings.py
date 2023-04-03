7a2883c186fa149c04370e5b723e740c9db25972
RyandTeague <ryanteague@hotmail.co.uk>
2023-04-03 12:07:38 +0000

    update requirements

--- drf_tsk/settings.py
+++ drf_tsk/settings.py
@@ -36,7 +36,8 @@ REST_FRAMEWORK = {
     # )],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        #'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
